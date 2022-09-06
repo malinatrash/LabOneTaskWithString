b315ef3276e21f4382a37dd20b9bc6a19d87296f
Павел Наумов <malinatrashh@gmail.com>
2022-09-06 18:13:36 +0800

    Realesed

---
+++ LabOneTaskString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LabOneTaskString
+{
+    public class Logic
+    {
+        public static string Reverse(string sentense)
+        {   
+            string[] wordsOfSentense = sentense.Split(' ');
+            Array.Reverse(wordsOfSentense);
+
+            string reversedSentense = "";
+
+            foreach (var word in wordsOfSentense)
+            {
+                reversedSentense += word + " ";
+            }
+            return (reversedSentense);
+        }
+    }
+    class Programm
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter a sentense: ");
+            var sentense = Console.ReadLine();
+
+            var reversedSentense = Logic.Reverse(sentense);
+
+            Console.WriteLine(reversedSentense);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LabOneTaskStringTests/LogicTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LabOneTaskString;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabOneTaskString.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void IsSentenseDoesNotContainSpaces()
+        {
+            var sentense = "abcdefg";
+
+            var reversedSentense = Logic.Reverse(sentense);
+
+            Assert.AreEqual("abcdefg", reversedSentense);
+        }
+
+        [TestMethod()]
+        public void IsSentenseContainSpaces()
+        {
+            var sentense = "abc def";
+
+            var reversedSentense = Logic.Reverse(sentense);
+
+            Assert.AreEqual("def abc", reversedSentense);
+        }
+
+        [TestMethod()]
+        public void IsSentenseIsRandomString()
+        {
+            var sentense = "i hate mac";
+
+            var reversedSentense = Logic.Reverse(sentense);
+
+            Assert.AreEqual("mac hate i ", reversedSentense);
+        }
+    }
+}
